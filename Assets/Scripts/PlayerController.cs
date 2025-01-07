915a6026efe70d17de4b76b1265709996ed36529
EmreBebis <emrebebi2015@hotmail.com>
2025-01-08 00:04:12 +0300

    playercontroller script
    ship
    background

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Hareket hızı
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Klavye girişlerini al
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); // A/D veya Sol/Sağ okları
+        float verticalInput = Input.GetAxisRaw("Vertical"); // W/S veya Yukarı/Aşağı okları
+
+        // Hareketi hesapla
+        movement = new Vector2(horizontalInput, verticalInput).normalized * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        // Hareketi uygula
+        rb.linearVelocity = movement;
+    }
+}
